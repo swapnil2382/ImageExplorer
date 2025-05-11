982b38c92f3aab7841a0824f59c8484c792cf429
swapnil patil <swapnil20002382@gmail.com>
2025-05-11 17:43:53 +0530

    updated

--- client/src/components/ImageGrid.js
+++ client/src/components/ImageGrid.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Download, Eye } from 'lucide-react';
 import './ImageGrid.css';
 
-const ImageGrid = ({ images, selectedImages, toggleSelect }) => {
+const ImageGrid = ({ images }) => {
   return (
     <div className="image-grid">
       {images.map(img => (
@@ -11,12 +12,27 @@ const ImageGrid = ({ images, selectedImages, toggleSelect }) => {
             alt={img.alt_description || "Image"}
             className="image"
           />
-          <input
-            type="checkbox"
-            checked={selectedImages.includes(img.urls.small)}
-            onChange={() => toggleSelect(img.urls.small)}
-            className="select-checkbox"
-          />
+          <div className="overlay">
+            <a
+              href={img.links.download}
+              download
+              target="_blank"
+              rel="noopener noreferrer"
+              className="download-button"
+            >
+              <Download size={16} />
+              <span>Download</span>
+            </a>
+            <a
+              href={img.urls.full}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="view-button"
+            >
+              <Eye size={16} />
+              <span>View Full</span>
+            </a>
+          </div>
           {img.description && (
             <div className="image-description">
               {img.description.length > 60 ? img.description.substring(0, 60) + '...' : img.description}
